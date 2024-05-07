a88c7357d669d2b5fb4505b45627e9aba78aa8f1
HjortheimerDaniel <147121561+HjortheimerDaniel@users.noreply.github.com>
2024-05-07 10:38:42 +0900

    Update main.cpp

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -77,8 +77,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		
 		Novice::DrawTriangle((int)screenVertices[0].x, (int)screenVertices[0].y, (int)screenVertices[1].x, (int)screenVertices[1].y, (int)screenVertices[2].x, (int)screenVertices[2].y, RED, kFillModeSolid);
+		
 		ownMatrix4x4->VectorScreenPrintf(0, 0, cross, "Cross");
-
+		
 		///
 		/// ↑描画処理ここまで
 		///
